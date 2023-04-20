0299faa2632020f9585c094debaf7535ec22007f
Adam Pilewski <apilewski01@gmail.com>
2023-04-20 19:25:03 +0200

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Books</title>
+        <meta name="description" content="Books App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <p>Hello world!</p>
+      </main>
+    </>
+  );
+};
+
+export default Home;
+
+
---
+++ src/server/auth.ts
@@ -0,0 +1,80 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import bcrypt from 'bcrypt';
+import { type GetServerSidePropsContext } from "next";
+import {
+  getServerSession,
+  type DefaultSession,
+  type NextAuthOptions
+} from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "~/server/db";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
+
+export const authOptions: NextAuthOptions = {
+  callbacks: {
+    session({ session, token }) {
+      if (session?.user && token.sub) {
+        session.user.id = token.sub;
+      }
+      // console.log('TOKEN:', token)
+      // console.log('SESSION:', session)
+      return session
+    },
+    jwt: async ({ user, token }) => {
+      if (user) {
+        token.uid = user.id;
+      }
+      return token;
+    },
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid credentials.");
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email
+          }
+        })
+        if (!user || !user.password) {
+          throw new Error("Invalid credentials.");
+        }
+        const correctPassword = await bcrypt.compare(credentials.password, user.password)
+        if (!correctPassword) {
+          throw new Error("Invalid credentials.");
+        }
+        return user
+      }
+    })
+  ],
+  session: {
+    strategy: 'jwt'
+  },
+  secret: process.env.NEXTAUTH_SECRET
+};
+
+export const getServerAuthSession = (ctx: {
+  req: GetServerSidePropsContext["req"];
+  res: GetServerSidePropsContext["res"];
+}) => {
+  return getServerSession(ctx.req, ctx.res, authOptions);
+};
+
+export const register = () => {
+  // TODO:
+}
